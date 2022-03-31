267dc2291d0ae3b3f2c8c01f3a2356e3c7069e3d
Jay Ji <jiqiang90@hotmail.com>
2022-03-31 15:38:19 +1300

    draft subscription (#106)
    * draft subscription
    
    * Update docs
    
    Co-authored-by: James Bayly <bayly.james@gmail.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -201,6 +201,7 @@ function getSidebar(locale, language){
         `${locale}/run_publish/query.md`,
         `${locale}/run_publish/graphql.md`,
 		    `${locale}/run_publish/aggregate.md`,
+        `${locale}/run_publish/subscription.md`,
         `${locale}/run_publish/references.md`,
       ]
     },
