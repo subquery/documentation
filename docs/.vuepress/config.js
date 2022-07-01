f9fb7345075abf39932da82775b4b3d06223d383
ZeelPatel1102 <zeel.patel@subquery.network>
2022-07-01 10:19:29 +0530

    Add Glossary page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -351,6 +351,12 @@ function getSidebar(locale, language) {
         collapsable: true,
         children: [`${locale}/faqs/faqs.md`],
       },
+      {
+        title: "Glossary",
+        path: `${locale}/glossary/glossary`,
+        collapsable: true,
+        children: [`${locale}/glossary/glossary.md`],
+     },
     ],
   };
 }
