94bec49f4bfc02e27cfc38939bbcba52cc17b1be
cyrbuzz <cyrbuzz@foxmail.com>
2025-05-07 10:43:57 +0800

    chore: replace gtm

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -22,6 +22,15 @@ export default defineUserConfig({
         content: "user-scalable=no, width=device-width, initial-scale=1.0",
       },
     ],
+    [
+      "script",
+      {},
+      `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PF5M4WSK');`,
+    ],
   ],
   locales: {
     "/": {
@@ -126,9 +135,8 @@ export default defineUserConfig({
     registerComponentsPlugin({
       componentsDir: "./docs/.vuepress/components",
     }),
-    googleAnalyticsPlugin({
-      id: "G-MY90N76MNK",
-    }),
+
+    ,
     redirectPlugin({
       config: (app) => {
         const redirects: Record<string, string> = {
